96561ba3302f584a842ea2e7d2ca85da49028dcc
laptop_fabian <fabianxd123456@hotmail.com>
2025-05-22 13:17:14 -0700

    fix

--- Pagina_Prenacional_SNTE-prueba/Disciplinas/Funciones.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.boton-imagen').forEach(function(boton) {
-  boton.addEventListener('click', function() {
-    // Busca el siguiente elemento hermano con la clase 'opciones'
-    var opciones = this.nextElementSibling;
-    if (opciones && opciones.classList.contains('opciones')) {
-      opciones.style.display = opciones.style.display === 'none' ? 'block' : 'none';
-    }
-  });
-});
